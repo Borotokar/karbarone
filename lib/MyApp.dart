faba30901579addfba14e0a909141092149fdd04
Borotokar <hsms4050@gmail.com>
2025-11-30 12:17:19 -0800

    Update MyApp.dart

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -22,10 +22,12 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => 
+_MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+class _MyAppState extends State<MyApp> 
+with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
